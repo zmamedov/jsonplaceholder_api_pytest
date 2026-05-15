1c78d6128fb6ae06093a7ded786a68dd78445c68
Zaur Mamedov <zaurmd1996@gmail.com>
2026-05-15 21:11:54 +0300

    Add test_get_all_comments

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,13 +1,22 @@
 import requests
 
-url = "https://jsonplaceholder.typicode.com/posts"
 
-
-def test_get_all_posts():
+def test_get_all_posts(base_url):
     payload = {}
     headers = {}
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", base_url, headers=headers, data=payload)
+    response_time_ms = response.elapsed.total_seconds() * 1000
+
+    assert response.status_code == 200
+    assert response_time_ms < 500
+
+
+def test_get_all_comments(base_url):
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", base_url, headers=headers, data=payload)
     response_time_ms = response.elapsed.total_seconds() * 1000
 
     assert response.status_code == 200
