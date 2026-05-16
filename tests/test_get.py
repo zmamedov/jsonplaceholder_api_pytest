7e36a25ff164e78bd8cf034b1d549e04913495e0
Zaur Mamedov <zaurmd1996@gmail.com>
2026-05-16 20:06:42 +0300

    Add logic posts_api.py

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,16 +1,25 @@
-import requests
+from jsonplaceholder-api-pytest.api.posts_api import PostsApi
 
 
 def test_get_all_posts(base_url):
-    payload = {}
-    headers = {}
-    url = f"{base_url}/posts"
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-    response_time_ms = response.elapsed.total_seconds() * 1000
+    posts_api = PostsApi(base_url)
+    response = posts_api.get_all_posts()
 
     assert response.status_code == 200
-    assert response_time_ms < 500
+    # response_time_ms = response.elapsed.total_seconds() * 1000
+    # assert response_time_ms < 500
+    # assert "application/json" in response.headers["Content-Type"]
+
+    # # Проверка структуры данных ответа
+    # posts = response.json()
+    # assert isinstance(posts, list)
+    # assert len(posts) > 0
+
+    # # Проверка обязательных полей на примере первого поста
+    # first_post = posts
+    # assert "id" in first_post
+    # assert "title" in first_post
+    # assert isinstance(first_post["id"], int)
 
 
 # def test_get_all_comments(base_url):
