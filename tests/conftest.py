1cb0aa706601cc9d7b8c3b6c6b6e01a93b02c7f9
Zaur Mamedov <zaurmd1996@gmail.com>
2026-05-24 19:29:19 +0300

    Add tests for endpoints albums, photos, todos, users

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 
 from jsonplaceholder_api_pytest.api.comments_api import CommentsApi
 from jsonplaceholder_api_pytest.api.posts_api import PostsApi
+from jsonplaceholder_api_pytest.api.photos_api import PhotosApi
+from jsonplaceholder_api_pytest.api.albums_api import AlbumsApi
+from jsonplaceholder_api_pytest.api.todos_api import TodosApi
+from jsonplaceholder_api_pytest.api.users_api import UsersApi
+
 
 load_dotenv()
 
@@ -21,3 +26,23 @@ def posts_api(base_url):
 @pytest.fixture(scope="session")
 def comments_api(base_url):
     return CommentsApi(base_url)
+
+
+@pytest.fixture(scope="session")
+def photos_api(base_url):
+    return PhotosApi(base_url)
+
+
+@pytest.fixture(scope="session")
+def albums_api(base_url):
+    return AlbumsApi(base_url)
+
+
+@pytest.fixture(scope="session")
+def todos_api(base_url):
+    return TodosApi(base_url)
+
+
+@pytest.fixture(scope="session")
+def users_api(base_url):
+    return UsersApi(base_url)
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3,7 +3,7 @@ from jsonplaceholder_api_pytest.api.response_assert import ResponseAssert
 
 def test_get_all_posts(posts_api):
     response = posts_api.get_all_posts()
-    
+
     (ResponseAssert(response)
      .status_code_should_be(200)
      .content_type_should_be_json()
@@ -17,3 +17,40 @@ def test_get_all_comments(comments_api):
      .status_code_should_be(200)
      .content_type_should_be_json()
      .should_match_basic_schema())
+
+
+def test_get_all_photos(photos_api):
+    response = photos_api.get_all_photos()
+
+    (ResponseAssert(response)
+     .status_code_should_be(200)
+     .content_type_should_be_json()
+     .should_match_basic_schema())
+
+
+def test_get_all_albums(albums_api):
+    response = albums_api.get_all_albums()
+
+    (ResponseAssert(response)
+     .status_code_should_be(200)
+     .content_type_should_be_json()
+     .should_match_basic_schema())
+
+
+def test_get_all_todos(todos_api):
+    response = todos_api.get_all_todos()
+
+    (ResponseAssert(response)
+     .status_code_should_be(200)
+     .content_type_should_be_json()
+     .should_match_basic_schema())
+    
+
+def test_get_all_users(users_api):
+    response = users_api.get_all_users()
+
+    (ResponseAssert(response)
+     .status_code_should_be(200)
+     .content_type_should_be_json()
+     .should_match_basic_schema())
+    
