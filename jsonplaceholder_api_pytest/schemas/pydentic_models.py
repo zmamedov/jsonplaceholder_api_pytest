d1bb957f8f53d415c30bb4c70529432150ce22d8
Zaur Mamedov <zaurmd1996@gmail.com>
2026-05-31 19:48:54 +0300

    Add strict validation

--- jsonplaceholder_api_pytest/schemas/pydentic_models.py
+++ jsonplaceholder_api_pytest/schemas/pydentic_models.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class PostModel(BaseModel):
+    """Validation scheme for one post with endpoint /posts/{id}."""
+
+    model_config = ConfigDict(strict=True)
+
     user_id: int = Field(alias='userId')
     id: int
     title: str
