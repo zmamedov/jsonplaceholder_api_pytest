b3568ea59a918860cbe7edc2d7d0cbb714c71c60
Zaur Mamedov <zaurmd1996@gmail.com>
2026-06-13 17:23:42 +0300

    Fix comments to smoke tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,6 +2,10 @@ from jsonplaceholder_api_pytest.api.assertions.response_assert import ResponseAs
 
 
 def test_get_all_posts(posts_api):
+    """
+    Test-case 1: Get all posts.
+    Check response and validation scheme.
+    """
     response = posts_api.get_all_posts()
 
     (ResponseAssert(response)
@@ -11,6 +15,10 @@ def test_get_all_posts(posts_api):
 
 
 def test_get_all_comments(comments_api):
+    """
+    Test-case 2: Get all comments.
+    Check response and validation scheme.
+    """
     response = comments_api.get_all_comments()
 
     (ResponseAssert(response)
@@ -20,6 +28,10 @@ def test_get_all_comments(comments_api):
 
 
 def test_get_all_photos(photos_api):
+    """
+    Test-case 3: Get all photos.
+    Check response and validation scheme.
+    """
     response = photos_api.get_all_photos()
 
     (ResponseAssert(response)
@@ -29,6 +41,10 @@ def test_get_all_photos(photos_api):
 
 
 def test_get_all_albums(albums_api):
+    """
+    Test-case 4: Get all albums.
+    Check response and validation scheme.
+    """
     response = albums_api.get_all_albums()
 
     (ResponseAssert(response)
@@ -38,6 +54,10 @@ def test_get_all_albums(albums_api):
 
 
 def test_get_all_todos(todos_api):
+    """
+    Test-case 5: Get all todos.
+    Check response and validation scheme.
+    """
     response = todos_api.get_all_todos()
 
     (ResponseAssert(response)
@@ -47,6 +67,10 @@ def test_get_all_todos(todos_api):
     
 
 def test_get_all_users(users_api):
+    """
+    Test-case 6: Get all users.
+    Check response and validation scheme.
+    """
     response = users_api.get_all_users()
 
     (ResponseAssert(response)
