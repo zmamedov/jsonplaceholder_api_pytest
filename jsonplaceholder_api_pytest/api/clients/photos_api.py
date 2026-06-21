240242ca4bbae8fb1047f8f8cfadaf565cf0e034
Zaur Mamedov <zaurmd1996@gmail.com>
2026-06-21 17:45:48 +0300

    Add pagination to tests GET all photos and comments

--- jsonplaceholder_api_pytest/api/clients/photos_api.py
+++ jsonplaceholder_api_pytest/api/clients/photos_api.py
@@ -5,10 +5,25 @@ import requests
 
 
 class PhotosApi:
+    """
+    API client for interacting with /photos endpoint.
+    """
+
     def __init__(self, base_url: str):
+        """
+        Initializes the client with the core endpoints.
+
+        :param base_url: The base URL of the target API environment.
+        """
         self.url = f"{base_url}/photos"
 
-    # Send GET request to get all photos
-    def get_all_photos(self):
-        return requests.get(self.url)
-    
+    def get_all_photos(self, page: int = None, limit: int = None):
+        """
+        GET /photos?_page={page}&_limit={limit}
+        Retrieves a paginated list of photos.
+
+        :param page: The page number to retrieve (e.g., 1).
+        :param limit: The number of photos to return per page (e.g., 10).
+        :return: A requests.Response object containing an array of photos.
+        """
+        return requests.get(f"{self.url}?_page={page}&_limit={limit}")
